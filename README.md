9a7bf45df36e745c0872c530f0eefd6c7d820d44
Castleberry <cbcastleberry@summithealthcare.com>
2018-01-02 13:38:34 -0500

    Chapter 3 Complete
    added a login page with flask webforms

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ A repository for working through the flask mega tutorial found here:
 https://blog.miguelgrinberg.com/
 
 ## Update Log
-* 12/29/17 - Chapter 2 complete and commited to master branch. I should create new branches for future chapters.
+* 12/29/17  - Chapter 2 complete and commited.
+* 1/2/18    - Chapter 3 Complete and commited.
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
+# Index Page
 @app.route('/')
 @app.route('/index')
 def index():
@@ -17,3 +19,15 @@ def index():
     ]
     return render_template('index.html', title="Home", 
                             user=user, posts= posts)
+
+
+#Login Page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign in', form=form)
+
